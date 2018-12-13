de27feae002ed79d08062682d66dee0810dc3ac6
benji dalton <daylightsavings@gmail.com>
2018-12-13 00:03:06 -0500

    begin styling/layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang='pug'>
+  #app
+    nav#app-nav
+      .title Benji Dalton
+      .links
+        router-link.link(to='/') Home
+        router-link.link(to='/') Posts
+        router-link.link(to='/') Music
+        router-link.link(to='/about') About
+    main#main-layout
+      router-view
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url(https://fonts.googleapis.com/css?family=Roboto);
+@import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
+@import url(https://fonts.googleapis.com/css?family=Lobster);
+@import 'assets/style/reset';
+body {
+  font-family: Roboto;
+  background-image: url('assets/image/bg.jpg');
+  background-size: cover;
+  background-attachment: fixed;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+a {
+  color: #fff;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
   }
+}
+#app-nav {
+  width: 100%;
+  height: 2.6rem;
+  background: rgba(66, 66, 66, 0.678);
+  .title {
+    float: left;
+    line-height: 2.8rem;
+    color: rgb(255, 255, 255);
+    padding-left: .5rem;
+    font-family: Lobster;
+    font-size: 1.2rem;
+  }
+  .links {
+    float: right;
+    padding-right: .5rem;
+  }
+  .link {
+    font-size: .78rem;
+    color: rgb(255, 255, 255);
+    line-height: 2.8rem;
+    letter-spacing: .8px;
+    margin: 0 .4rem;
+  }
+}
+#main-layout {
+  margin: 1.6rem;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,9 +2,8 @@
   div
     h2 posts
     div(v-for='post in posts')
-      article
-        hr
-        h3
+      article.entry
+        h3.post-title
           router-link(:to='{name: "post", params: {slug: post.slug}}')
             | {{post.title}}
         p {{post.preview}}
@@ -24,3 +23,20 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.entry {
+  padding: 1rem;
+  margin: 1.2rem auto;
+  background: rgba(37, 37, 37, 0.418);
+  color: whitesmoke;
+}
+.post-title {
+  margin-bottom: 1rem;
+  a {
+    font-size: 1.5rem;
+    font-family: Montserrat;
+    color: #fff;
+  }
+}
+</style>
